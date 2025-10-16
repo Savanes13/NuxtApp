72aecfa119336cd0d9692ddb67aed39c9ba0dff0
Savanes13 <ilzz43373@gmail.com>
2025-10-16 12:07:58 +0300

    подключение шрифта

--- app.vue
+++ app.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>
+  <div class="app">
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
   </div>
 </template>
+
+<style lang="scss">
+@use "./style/variables/font" as font;
+
+.app {
+  font-family: font.$font-Gilroy;
+  // min-height: 100dvh;
+}
+</style>
