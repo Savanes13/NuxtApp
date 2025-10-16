8e7783622941aa65e00f134e91d63ebade500043
Savanes13 <ilzz43373@gmail.com>
2025-10-16 13:48:21 +0300

    вывод блоков advantages стилизация

--- pages/index.vue
+++ pages/index.vue
@@ -8,11 +8,11 @@ const dataAdvantagesArray = ref<TAdvantagesArray | null>(null);
 const getAdvantages = async () => {
   try {
     const response = await getAdvantageData();
-    const advantages = response.map(item => ({
+    const advantages = response.map((item: any) => ({
       id: item.id,
       name: item.name,
       ...JSON.parse(item.value)
-    }))
+    }));
     dataAdvantagesArray.value = advantages;
     console.log(advantages)
   } catch (error) {
@@ -29,20 +29,39 @@ getAdvantages();
   >
     
     <div class="index-page__advantages-block">
-      <AdvantagesBlock
-        v-for="item in dataAdvantagesArray"
-        :key="item.id"
-        :image="item.icon"
-        :title="item.title"
-        :text="item.description"
-      />
+      <div class="title-advantages">
+        <p>Наши преимущества</p>
+      </div>
+      <div class="items-advantages">
+        <AdvantagesBlock
+          v-for="item in dataAdvantagesArray"
+          :key="item.id"
+          :image="item.icon"
+          :title="item.title"
+          :text="item.description"
+        />
+      </div>
     </div>
 
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+@use "@/style/variables/color.scss" as color;
+
 .index-page__advantages-block {
+  margin-bottom: 147px;
+}
+
+.title-advantages {
+  font-weight: 700;
+  font-size: 42px;
+  line-height: 97%;
+  color: color.$main_white;
+  margin-bottom: 80px;
+}
+
+.items-advantages {
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 32px;
