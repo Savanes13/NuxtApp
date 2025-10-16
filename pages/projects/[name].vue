6798474642d1e00bb191de83e31c6c77c9046de3
Savanes13 <ilzz43373@gmail.com>
2025-10-16 23:53:23 +0300

    крошки

--- pages/projects/[name].vue
+++ pages/projects/[name].vue
@@ -4,41 +4,59 @@ import { getProjectData } from '~/api/pages/project/apiProject';
 
 const route = useRoute();
 const slug = route.params.name as string;
+//типизировать
+const dataProjectArray = ref<any | null>(null);
+
+const breadCrumb = computed(() => [
+  {
+    name: 'Главная',
+    path: '/'
+  },
+  {
+    name: 'Проекты',
+    path: '/'
+  },
+  {
+    name: dataProjectArray.value?.customer || 'Загрузка...',
+    path: `/projects/${slug}`
+  }
+]);
 
 const getProjectDataArr = async () => {
   try {
     if(!slug) return;
     const response = await getProjectData(slug);
+    dataProjectArray.value = response;
   } catch (error) {
     console.log('ошибка при получении данных продукта', error);
   };
 };
 getProjectDataArr();
 
-const breadCrumb = [
-  {
-    name: "Главная",
-    path: "/"
-  },
-  {
-    name: "Проекты",
-    path: "/"
-  },
-  {
-    name: "Проекты",
-    path: "/"
-  },
-];
 </script>
 
 <template>
   <div class="project-page">
     <BreadCrumbs
+      class="bread-crumbs"
       :data="breadCrumb"
     />
+    <div class="project-page__content">
+      <div lang="info-block">
+
+      </div>
+
+      <div class="image-block">
+        
+      </div>
+
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped> 
-
+.bread-crumbs {
+  margin-top: 13px;
+  margin-bottom: 37px;
+}
 </style>
