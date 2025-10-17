4ae0b88ee565cba4655ad402e43bb218db9893ee
Savanes13 <ilzz43373@gmail.com>
2025-10-17 10:22:20 +0300

    блок преимуществ 2

--- pages/projects/[name].vue
+++ pages/projects/[name].vue
@@ -94,6 +94,32 @@ getProjectDataArr();
             </div>
           </div>
         </div>
+
+
+
+        <div class="info-block__customer-selection">
+          <div class="selection-title">
+            <p>Также лос выбирают за:</p>
+          </div>
+
+          <div class="selection-items selection-items--advanced">
+            <div class="selection-items__item-advanced">
+              <div class="circle circle-number">1</div>
+              <div class="advantages-сompany">
+                <div class="advantages-сompany__title">
+                  <p>ЭКОНОМИЧНОСТЬ</p>
+                </div>
+                <div class="advantages-сompany__text">
+                  <p>Приобретая станцию у производителя, вы добьетесь максимальной экономии</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+
+
       </div>
 
       <div class="image-block">
@@ -138,12 +164,17 @@ getProjectDataArr();
   margin-bottom: 12px;
 }
 
+.info-block__customer-selection {
+  margin-bottom: 24px;
+}
+
 .selection-title {
   font-weight: 700;
   font-size: 18px;
   line-height: 150%;
   color: #FFFFFFCC;
   margin-bottom: 19px;
+  text-transform: uppercase;
 }
 
 .selection-items {
@@ -166,11 +197,43 @@ getProjectDataArr();
   font-weight: 700;
 }
 
+.selection-items__item-advanced {
+  display: flex;
+  gap: 10px;
+}
+
+.advantages-сompany__title {
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 160%;
+  color: color.$main_white;
+}
+
+.advantages-сompany__text {
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 160%;
+  color: color.$main_white;
+}
+
 .circle {
   width: 12px;
   height: 12px;
   border: 2px solid color.$main_blue;
   border-radius: 50%;  
   background: transparent;
+}
+
+.circle-number {
+  flex: 0 0 24px;     
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 24px;
+  height: 24px;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 18px;
+  color: color.$main_blue;
 }
 </style>
